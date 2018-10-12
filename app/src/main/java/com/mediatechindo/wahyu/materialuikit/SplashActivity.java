ad61020f91bd72f78abd3637e7e28ddb7471c895
Shadab Azam <shadab.aazam@berylsystems.com>
2018-10-12 16:40:10 +0530

    Added Splash Page

---
+++ app/src/main/java/com/mediatechindo/wahyu/materialuikit/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.mediatechindo.wahyu.materialuikit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+                finish();
+            }
+        }, 2000);
+    }
+}
